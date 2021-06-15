d11ddfec86bd747b5bbfb2aaf7b777b0eefa296a
barkhatiwarii <48247221+barkhatiwarii@users.noreply.github.com>
2021-06-15 11:49:57 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,10 +5,11 @@ from api.v1.api import router as api_router
 
 app = FastAPI(title='Serverless Lambda FastAPI')
 
-app.include_router(api_router, prefix="/api/v1")
+#app.include_router(api_router, prefix="/api/v1")
 
 
-@app.get("/",  tags=["Endpoint Test"])
+#@app.get("/",  tags=["Endpoint Test"])
+@app.get("/")
 def main_endpoint_test():
     return {"message": "Welcome CI/CD Pipeline with GitHub Actions!"}
 
