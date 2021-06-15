a3bfe53e53b940dce280af65526f511cb441c5eb
barkhatiwarii <barkha1025@gmail.com>
2021-06-15 11:06:59 +0530

    updated

--- app/main.py
+++ app/main.py
@@ -1,10 +1,30 @@
-from mangum import Mangum
 from fastapi import FastAPI
+from mangum import Mangum
+import json
+from api.v1.api import router as api_router
 
-app = FastAPI()
+app = FastAPI(title='Serverless Lambda FastAPI')
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
+app.include_router(api_router, prefix="/api/v1")
 
-handler = Mangum(app=app)
+
+@app.get("/",  tags=["Endpoint Test"])
+def main_endpoint_test():
+    return {"message": "Welcome CI/CD Pipeline with GitHub Actions!"}
+
+
+# to make it work with Amazon Lambda, we create a handler object
+# handler = Mangum(app=app)
+def handler(event, context):
+    event['requestContext'] = {}  # Adds a dummy field; mangum will process this fine
+    
+    asgi_handler = Mangum(app)
+    response = asgi_handler(event, context)
+    json_data = event["queryStringParameters"] 
+    # user = json_data["user"]
+    return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps("hello")
+            }
+
